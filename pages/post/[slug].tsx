d5da7fc6d2765ab19069929244c2af2d57b8bd3e
miaawong <miawongdev@gmail.com>
2023-01-18 20:46:44 -0800

    prettier format

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,47 +1,51 @@
-import client from '../../client'
+import client from "../../client";
 
-const Post = ({post}) => {
-  console.log(post,'post')
+const Post = ({ post }) => {
+  console.log(post, "post");
   return (
     <article>
-      <h1 className='text-3xl'>{post?.slug?.current}</h1>
+      <h1 className="text-3xl">{post?.slug?.current}</h1>
 
       <p>{post?.body[0].children[0].text}</p>
     </article>
-  )
-}
+  );
+};
 
-//Next.js comes with a special function called getStaticProps 
-//that is called and returns props to the react component before 
-//rendering the templates in /pages. 
-//This is a perfect place for fetching the data you want for a page. 
-// You need to use this in tandem with another special function called 
+//Next.js comes with a special function called getStaticProps
+//that is called and returns props to the react component before
+//rendering the templates in /pages.
+//This is a perfect place for fetching the data you want for a page.
+// You need to use this in tandem with another special function called
 //getStaticPaths in order to tell Next.js upfront which posts exist.
 export async function getStaticPaths() {
   const paths = await client.fetch(
-    `*[_type == "post" && defined(slug.current)][].slug.current`
-  )
+    `*[_type == "post" && defined(slug.current)][].slug.current`,
+  );
 
   return {
-    paths: paths.map((slug) => ({params: {slug}})),
+    paths: paths.map((slug) => ({ params: { slug } })),
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps(context) {
+  console.log("hi yall");
   // It's important to default the slug so that it doesn't return "undefined"
-  const { slug = "" } = context.params
-  // * = select all docs 
-  // filter by type = post and find post that have the same slug 
+  const { slug = "" } = context.params;
+  // * = select all docs
+  // filter by type = post and find post that have the same slug
   // we have in the params
-  const post = await client.fetch(`
+  const post = await client.fetch(
+    `
     *[_type == "post" && slug.current == $slug][0]
-  `, { slug })
+  `,
+    { slug },
+  );
   return {
     props: {
-      post
-    }
-  }
+      post,
+    },
+  };
 }
 
-export default Post
+export default Post;
