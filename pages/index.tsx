c79064aa6614f6aaa4ecd0f8bf6ecf678f6b1f4a
miaawong <miawongdev@gmail.com>
2023-01-18 20:47:40 -0800

    add image and about me blurb add render for all posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import client from "../client";
+import groq from "groq";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+import imageUrlBuilder from "@sanity/image-url";
 
-export default function Home() {
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+const Home = ({ posts }) => {
   return (
     <>
       <Head>
@@ -18,102 +33,124 @@ export default function Home() {
         <div className={styles.description}>
           <h1 className="text-3xl font-normal">Adventures & Code </h1>
           <div className="flex gap-8">
-            <a
-              href="/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             home
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              home
             </a>
-            <a
-              href="/posts"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             posts
-            </a>
-            <a
-              href="/about"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             about
+            {/* <a href="/posts" target="_blank" rel="noopener noreferrer">
+              posts
+            </a> */}
+            <a href="/about" target="_blank" rel="noopener noreferrer">
+              about
             </a>
           </div>
         </div>
 
-        <div className="flex justify-center my-6 relative">
-          <Image
-            src="/rainier.jpeg"
-            alt="Mount Rainier"  
-            width={1000}
-            height={800}
-            priority
-          />
-          <div className='absolute left-4 translate-y-2/4'>
-          {/* <h1 className="text-3xl font-bold">DISCOVER WHAT YOU CAN DO</h1> */}
+        <div className="flex justify-between my-10 relative h-[800px]">
+          <div className="w-3/6 h-full relative">
+            <Image
+              src={"/mia.jpeg"}
+              alt="Picture of Mia hiking in Arizona"
+              fill
+              sizes={"100vh"}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+          <div className="w-3/6 ml-5 my-6 flex flex-col items-end justify-center text-end">
+            <p className="text-3xl font-bold">Hi! My name is Mia.</p>
+            <p className="text-3xl mt-2">Software Engineer, Adventure Seeker</p>
+            <p className=" mt-2">
+              I'm a self-taught Software Engineer with more than 3 years of
+              experience in design and frontend development.{" "}
+            </p>
+            <p className="mt-2">
+              I'm passionate building tools on the web and sharing my learning
+              journey.
+            </p>
+            <p className="mt-2">
+              When I'm not building new features or fixing bugs, you'll likely
+              find me outside skiing, hiking, or traveling.{" "}
+            </p>
           </div>
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+        <h1 className="text-3xl font-bold">recent posts</h1>
+        <div className="flex justify-between mt-4">
+          {posts.map(
+            (post: {
+              _id: Key | null | undefined;
+              title:
+                | string
+                | number
+                | boolean
+                | ReactElement<any, string | JSXElementConstructor<any>>
+                | ReactFragment
+                | ReactPortal
+                | null
+                | undefined;
+              body: {
+                children: {
+                  text:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | ReactFragment
+                    | ReactPortal
+                    | null
+                    | undefined;
+                }[];
+              };
+              mainImage: {
+                asset: {
+                  _ref: string;
+                };
+              }[];
+            }) => {
+              const { title, body, mainImage } = post;
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+              return (
+                <div className={""} key={post._id}>
+                  {body && (
+                    <>
+                      <img
+                        src={urlFor(mainImage).width(400).height(300).url()}
+                        style={{ objectFit: "contain", borderRadius: "4px" }}
+                      />
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+                      <h3 className="text-xl font-bold mt-2">{title}</h3>
+                      <p>{body[0].children[0].text}</p>
+                    </>
+                  )}
+                </div>
+              );
+            },
+          )}
         </div>
       </main>
+      <footer className="h-36 bg-[#fafafa]">
+        <div className="max-w-[1200px] m-auto p-7">
+          <p className="text-xl">let's stay connected!</p>
+        </div>
+      </footer>
     </>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  // It's important to default the slug so that it doesn't return "undefined"
+
+  // * = select all docs
+  // filter by type = post and find post that have the same slug
+  // we have in the params
+  const posts = await client.fetch(groq`
+    *[_type == 
+      "post"]| order(_publishedAt desc)
+  `);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
+
+export default Home;
