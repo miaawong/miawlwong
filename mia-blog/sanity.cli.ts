4275300d72765caec0614664e61aa0f90dca73a5
miaawong <miawongdev@gmail.com>
2023-01-22 14:49:40 -0800

    add packages for deployment

--- mia-blog/sanity.cli.ts
+++ mia-blog/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from '@sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'hdplt38c',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
