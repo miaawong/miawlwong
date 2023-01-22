ce7ae56a2e9720c44083339c472099680dd99b87
miaawong <miawongdev@gmail.com>
2023-01-22 14:52:06 -0800

    add post layout

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,13 +1,69 @@
 import client from "../../client";
+import { Post } from "../../types";
+import Nav from "../../components/Nav";
+import { urlFor } from "../../utils";
+import Image from "next/image";
+import Footer from "../../components/Footer";
+import Head from "next/head";
 
-const Post = ({ post }) => {
-  console.log(post, "post");
+const Post = ({ post }: { post: Post }) => {
+  if (!post) return <div>Loading...</div>;
+  const { title, body, mainImage, name, createdAt } = post;
+  const date = new Date(createdAt);
+  const year = date.getFullYear();
+  const month = date.toLocaleString("default", { month: "long" });
+  const day = date.getDate();
+
   return (
-    <article>
-      <h1 className="text-3xl">{post?.slug?.current}</h1>
-
-      <p>{post?.body[0].children[0].text}</p>
-    </article>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content={
+            (post &&
+              post.body?.length > 0 &&
+              body[0]?.children[0].text.substring(0, 100)) ||
+            title
+          }
+        />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content={
+            (post &&
+              post.body?.length > 0 &&
+              body[0]?.children[0].text.substring(0, 100)) ||
+            title
+          }
+        />
+        <meta property="og:image" content={urlFor(mainImage).url()} />
+      </Head>
+      <main className={"max-w-screen-xl m-auto p-6 min-h-[88vh]"}>
+        <Nav />
+        <article className={"mt-10 mb-20"}>
+          <div className="mb-6">
+            <h1 className="text-3xl font-bold mb-2">{title}</h1>
+            <p>
+              Published on {month} {day}, {year} by {name}
+            </p>
+          </div>
+          <div className="flex flex-col m-auto mb-10">
+            <Image
+              src={urlFor(mainImage).url()}
+              alt={`${title}`}
+              width={500}
+              height={300}
+              className="m-auto rounded w-[100%] lg:w-[80%] max-h-[700px] object-cover"
+              quality="100"
+              priority={true}
+            />
+          </div>
+          {body && body.length > 0 && <p>{body[0]?.children[0].text}</p>}
+        </article>
+      </main>
+      <Footer />
+    </>
   );
 };
 
@@ -23,13 +79,14 @@ export async function getStaticPaths() {
   );
 
   return {
-    paths: paths.map((slug) => ({ params: { slug } })),
+    paths: paths.map((slug: { current: string }) => ({ params: { slug } })),
     fallback: true,
   };
 }
 
-export async function getStaticProps(context) {
-  console.log("hi yall");
+export async function getStaticProps(context: {
+  params: { slug?: "" | undefined };
+}) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.params;
   // * = select all docs
@@ -38,6 +95,16 @@ export async function getStaticProps(context) {
   const post = await client.fetch(
     `
     *[_type == "post" && slug.current == $slug][0]
+     { 
+      mainImage,
+      title, 
+      "name": author->name,
+      "categories": categories[]->title,
+      body,
+     slug,
+     'createdAt': _createdAt
+      
+    }
   `,
     { slug },
   );
