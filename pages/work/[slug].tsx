e86d14c28b7fab5d2187fd69f1d71ff8bcfcbb6f
miaawong <miawongdev@gmail.com>
2023-02-19 13:25:16 -0800

    remove ternary

--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -11,7 +11,7 @@ import { AiOutlineGithub } from "react-icons/ai";
 const Project = ({ project }: { project: Project }) => {
   if (!project) return <div>Loading...</div>;
   const { title, body, github } = project;
-  const mainImage = project?.mainImage;
+  const mainImage = project.mainImage;
 
   return (
     <>
